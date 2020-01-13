e8ff256d8d482eea45bea2fe8b4c8a9de0eebe7d
lamo0257 <lamo0257@algonquinlive.com>
2020-01-12 20:11:46 -0500

    did the math in script.js

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 var name = 'Sherri'
 console.log(`Hello my name is ${name}`)
+
+const a = 5
+const b = 2
+const c = 10
+const d = 7
+const e = 50
+function multiply (a, b) {
+  return a * b
+}
+function multiply (c, a) {
+  return c * a
+}
+function divide (e, a) {
+  return e / a
+}
+function subtract (c, d) {
+  return c - d
+}
